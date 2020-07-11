1e526495446540044a899a21bf39ad1f1d3e7199
hirokoshiroyama <66316040+hirokoshiroyama@users.noreply.github.com>
2020-07-11 19:13:14 +0900

    AlertDialog

--- app/src/main/java/com/example/ui/MainActivity.kt
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -2,19 +2,50 @@ package com.example.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.Long4
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1.setOnClickListener {
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+    }
 
+    override fun onClick(v: View) {
+        if (v.id == R.id.button1) {
             textView.text = editText.text.toString()
+        } else if (v.id == R.id.button2) {
+            showAlertDialog()
         }
     }
+
+    private fun showAlertDialog(){
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("タイトル")
+        alertDialogBuilder.setMessage("メッセージ")
+
+        alertDialogBuilder.setPositiveButton("肯定"){dialog, which ->
+            Log.d("UI_PARTS","肯定ボタン")
+        }
+
+        alertDialogBuilder.setNeutralButton("中立"){_,_->
+            Log.d("UT_PARTS","中立ボタン")
+        }
+
+        alertDialogBuilder.setNegativeButton("否定"){_,_->
+            Log.d("UT_PARTS","否定ボタン")
+        }
+    val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
 }
 
 
