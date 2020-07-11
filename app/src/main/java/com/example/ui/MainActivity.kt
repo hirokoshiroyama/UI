ecd8c31ae3d3e204977e460c778ac87657e355cf
hirokoshiroyama <66316040+hirokoshiroyama@users.noreply.github.com>
2020-07-11 20:11:45 +0900

    TimePickerDialog

--- app/src/main/java/com/example/ui/MainActivity.kt
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.ui
 
+import android.app.TimePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.Long4
 import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.view.View
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
         button1.setOnClickListener(this)
         button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -23,29 +24,42 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
             textView.text = editText.text.toString()
         } else if (v.id == R.id.button2) {
             showAlertDialog()
+        } else if (v.id == R.id.button3) {
+            showTimePickerDialog()
         }
     }
 
-    private fun showAlertDialog(){
+    private fun showAlertDialog() {
         val alertDialogBuilder = AlertDialog.Builder(this)
         alertDialogBuilder.setTitle("タイトル")
         alertDialogBuilder.setMessage("メッセージ")
 
-        alertDialogBuilder.setPositiveButton("肯定"){dialog, which ->
-            Log.d("UI_PARTS","肯定ボタン")
+        alertDialogBuilder.setPositiveButton("肯定") { dialog, which ->
+            Log.d("UI_PARTS", "肯定ボタン")
         }
 
-        alertDialogBuilder.setNeutralButton("中立"){_,_->
-            Log.d("UT_PARTS","中立ボタン")
+        alertDialogBuilder.setNeutralButton("中立") { _, _ ->
+            Log.d("UT_PARTS", "中立ボタン")
         }
 
-        alertDialogBuilder.setNegativeButton("否定"){_,_->
-            Log.d("UT_PARTS","否定ボタン")
+        alertDialogBuilder.setNegativeButton("否定") { _, _ ->
+            Log.d("UT_PARTS", "否定ボタン")
         }
-    val alertDialog = alertDialogBuilder.create()
+        val alertDialog = alertDialogBuilder.create()
         alertDialog.show()
     }
 
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                Log.d("UT_PARTS", "$hour:$minute")
+            },
+            13, 0, true
+        )
+        timePickerDialog.show()
+    }
 }
+
 
 
